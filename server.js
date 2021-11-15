7657fe5e59d011e4357103a083e7f872ea982792
ashokkumar9892 <ashokkumar79892@gmail.com>
2021-11-14 21:55:34 -0500

    working now

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require('express')
+var express = require("express");
 const { default: chat } = require("./controllers/chat");
 
-const app=express();
-const http=require("http").createServer(app);
-app.get('/api',(req,res)=>{
-    res.json({
-        data: "hello from node js api"
-    })
-})
-http.listen(8000,()=>console.log("server is running on 80000"))
-chat(http)
+const app = express();
+const http = require("http").createServer(app);
+app.get("/api", (req, res) => {
+  res.json({
+    data: "hello from node js api",
+  });
+});
+http.listen(8800, () => console.log("server is running on 8800"));
+chat(http);
